5db5e8d8527445b9683addf319459e80e9a9c356
Abdulrasheed313 <rasheedmumna@gmail.com>
2025-03-10 05:51:59 +0500

    thgird

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+
+
+
 function add(e){
   
      var addInp = document.getElementById('inp');
      addInp.value += e
+   
 }
 
 
@@ -14,10 +18,14 @@ function eqal(){
     
     addInp.value = shows
   }
+
+  
 }
-function clear(){
+
+function clears(){
   var addInp = document.getElementById('inp');
-  addInp.value = addInp.value.slice(0, -1);
+  addInp.value =  addInp.value.slice(0 , -1) ;
+
 }
 
 function clearall(){
@@ -29,21 +37,21 @@ function clearall(){
       var addInp = document.getElementById('inp');
       var buttons = document.getElementsByTagName('button');
       
-      // Check if the input field is enabled, if true, disable them all
+   
       if (!addInp.disabled) {
-        // Disable the input field
+       
         addInp.disabled = true;
         
-        // Loop through all buttons and disable them
+       
         for (var i = 0; i < buttons.length; i++) {
           buttons[i].disabled = true;
         }
       }
       else {
-        // Enable the input field
+        
         addInp.disabled = false;
         
-        // Loop through all buttons and enable them
+        
         for (var i = 0; i < buttons.length; i++) {
           buttons[i].disabled = false;
         }
